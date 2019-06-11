7747efee740fad26e1a9f99664397970308e6497
parkcheolhan <parkcheolhan@bluedigm.com>
2019-06-11 09:24:55 +0900

    reset

--- jobs/market/templates/data/properties.sh
+++ jobs/market/templates/data/properties.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-export NAME='<%= name%>'
-export JOB_INDEX=<%= name %>
+# job template binding variables
+# job name & index of this VM within cluster
+# e.g. JOB_NAME=redis, JOB_INDEX=0
+export NAME='<%= name %>'
+export JOB_INDEX=<%= index %>
+# full job name, like redis/0 or webapp/3
 export JOB_FULL="$NAME/$JOB_INDEX"
